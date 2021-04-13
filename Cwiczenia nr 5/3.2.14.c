0892d01929804a6379c7ed0b8735942b4ea04a54
uwmkzacharewicz <karolzach87@gmail.com>
2021-04-13 22:16:51 +0200

    Update 3.2.14.c

--- Cwiczenia nr 5/3.2.14.c
+++ Cwiczenia nr 5/3.2.14.c
@@ -1,32 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <stdbool.h>
 
+int sgn_x1(int x);
+int sgn_x2(int x);
 
-int fun1(int a)
-{
-    return a * 2;
-}
-
-int fun2(int b)
-{
-    return b * 3;
-}
+int fun1(int a);
+int fun2(int b);
 
 typedef int(*wsk_fun)(int);
 
-// bool wykonaj(int(*wsk_fun)(int), int(*wsk_fun)(int), unsigned int n)
-bool wykonaj(wsk_fun pierwsza, wsk_fun druga, unsigned int n)
-{
-    for (unsigned int i = 0; i <= n; i++)
-        if (pierwsza(i) != druga(i))
-            return false;       // jezeli nie sa rowne zwroc false
-
-    // jezeli wszystkie wywolania funkcji sa rowne
-    // zwroc true
-    return true;
-}
+// bool wykonaj(int(*wsk_fun)(int), int(*wsk_fun)(int), unsigned int n);
+bool wykonaj(wsk_fun pierwsza, wsk_fun druga, unsigned int n);
 
 int main()
 {
@@ -38,15 +23,49 @@ int main()
         false,  w przeciwnym wypadku.
     */
 
-    printf("\n\n--------------------  3.2.14  ---------------------");
+    printf("\n\n-----------------  3.2.14  ---------------------\n\n");
 
-    printf("\n\nWYNIK: %d", wykonaj(fun1,fun2,10));
-    printf("\n\nWYNIK: %d", wykonaj(fun2,fun2,5));
-    printf("\n\nWYNIK: %d", wykonaj(fun2,fun1,7));
-    printf("\n\nWYNIK: %d", wykonaj(fun1,fun1,2));
+    printf("\nWYNIK: %d", wykonaj(fun1,fun2,10));
+    printf("\nWYNIK: %d", wykonaj(fun2,fun2,5));
+    printf("\nWYNIK: %d", wykonaj(fun2,fun2,10));
+    printf("\nWYNIK: %d", wykonaj(fun1,fun2,10));
+
+    printf("\n\n\nWYNIK: %d", wykonaj(sgn_x1,sgn_x2,5));
+    printf("\nWYNIK: %d", wykonaj(sgn_x1,sgn_x2,9));
+    printf("\nWYNIK: %d", wykonaj(sgn_x1,sgn_x2,10));
 
     printf("\n\n------------------------------------------------\n\n");
 
     return 0;
 }
 
+int fun1(int a)
+{
+    return a;
+}
+
+int fun2(int b)
+{
+    return b * 2;
+}
+
+int sgn_x1(int x)
+{
+  return (x > 10) - (x < 10);
+}
+
+int sgn_x2(int x)
+{
+  return (x > 20) - (x < 20);
+}
+
+bool wykonaj(wsk_fun pierwsza, wsk_fun druga, unsigned int n)
+{
+    for (unsigned int i = 0; i <= n; i++)
+        if (pierwsza(i) != druga(i))
+            return false;       // jezeli nie sa rowne zwroc false
+
+    // jezeli wszystkie wywolania funkcji sa rowne
+    // zwroc true
+    return true;
+}
